d71a7cd799f42f8911c6ed3379cff5a3c9b2f977
gizmo <72733125+gospino123@users.noreply.github.com>
2024-05-26 09:58:08 -0400

    Feat(nav-link):Use Blade directive conditionals

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,11 +1,11 @@
 @props(['active' => false, 'type' => 'a'])
 
-<?php if ($type === 'a'): ?>
+@if ($type === 'a')
   <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
     {{ $slot }}
   </a>
-<?php elseif ($type !== 'a'): ?>
+@elseif ($type !== 'a')
   <button class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
     {{ $slot }}
   </button>
-<?php endif ?>
+@endif
