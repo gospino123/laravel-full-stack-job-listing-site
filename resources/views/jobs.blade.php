dbedad5c2cb0bbdea7cb1c2d1ae4abfc9ebab31d
gizmo <72733125+gospino123@users.noreply.github.com>
2024-05-29 21:46:35 -0400

    Feat(jobs):Use data,wildcards,routes,Illuminate

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,10 +2,11 @@
     <x-slot:heading>
         Jobs
     </x-slot:heading>
-    <h2>Current Job Listings</h2>
+    <h2 class="font-bold text-lg">Current Job Listings</h2>
     <ul>
         @foreach ($jobs as $job)
             <li>
+                <a class="text-lime-300 hover:underline" href="/jobs/{{ $job['id'] }}"
                 <strong>{{$job['title']}}</strong>: Pays ${{$job['salary']}} per year.
             </li>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,21 +10,49 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [ 'jobs' => [
+    return view('jobs', 
+        [ 'jobs' => [
+            [
+                'title' => 'Director',
+                'salary' => '60,000',
+                'id' => 1,
+            ],
+            [
+                'title' => 'Programmer',
+                'salary' => '10,000',
+                'id' => 2,
+            ],
+            [
+                'title' => 'Teacher',
+                'salary' => '40,000',
+                'id' => 3,
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
         [
             'title' => 'Director',
             'salary' => '60,000',
+            'id' => 1,
         ],
         [
             'title' => 'Programmer',
             'salary' => '10,000',
+            'id' => 2,
         ],
         [
             'title' => 'Teacher',
             'salary' => '40,000',
+            'id' => 3,
         ],
-    ]
-    ]);
+    ];
+    
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
