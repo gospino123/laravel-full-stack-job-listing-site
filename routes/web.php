78ac17e3fb92d3d260a8f19cada596b70733903d
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-06 21:28:32 -0400

    Refactor(jobs):Convert var to class

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,27 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        'title' => 'Director',
-        'salary' => '60,000',
-        'id' => 1,
-    ],
-    [
-        'title' => 'Programmer',
-        'salary' => '10,000',
-        'id' => 2,
-    ],
-    [
-        'title' => 'Teacher',
-        'salary' => '40,000',
-        'id' => 3,
-    ],
-];
+class Jobs {
+    public static function all(): array {
+        return [
+            [
+                'title' => 'Director',
+                'salary' => '60,000',
+                'id' => 1,
+            ],
+            [
+                'title' => 'Programmer',
+                'salary' => '10,000',
+                'id' => 2,
+            ],
+            [
+                'title' => 'Teacher',
+                'salary' => '40,000',
+                'id' => 3,
+            ],
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home', [
@@ -27,15 +31,15 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', 
-        [ 'jobs' => $jobs
+        [ 'jobs' => Jobs::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
+Route::get('/jobs/{id}', function ($id) {
     
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Arr::first(Jobs::all(), fn($job) => $job['id'] == $id);
 
     return view('job', ['job' => $job]);
 });
