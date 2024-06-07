b83db342ea2fae3c09b1f400a23a2c905d29b00b
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-06 21:22:19 -0400

    Refactor(jobs):Bring up a lvl

--- routes/web.php
+++ routes/web.php
@@ -3,52 +3,37 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$jobs = [
+    [
+        'title' => 'Director',
+        'salary' => '60,000',
+        'id' => 1,
+    ],
+    [
+        'title' => 'Programmer',
+        'salary' => '10,000',
+        'id' => 2,
+    ],
+    [
+        'title' => 'Teacher',
+        'salary' => '40,000',
+        'id' => 3,
+    ],
+];
+
 Route::get('/', function () {
     return view('home', [
         'greeting' => 'HI',
     ]);
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', 
-        [ 'jobs' => [
-            [
-                'title' => 'Director',
-                'salary' => '60,000',
-                'id' => 1,
-            ],
-            [
-                'title' => 'Programmer',
-                'salary' => '10,000',
-                'id' => 2,
-            ],
-            [
-                'title' => 'Teacher',
-                'salary' => '40,000',
-                'id' => 3,
-            ],
-        ]
+        [ 'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'title' => 'Director',
-            'salary' => '60,000',
-            'id' => 1,
-        ],
-        [
-            'title' => 'Programmer',
-            'salary' => '10,000',
-            'id' => 2,
-        ],
-        [
-            'title' => 'Teacher',
-            'salary' => '40,000',
-            'id' => 3,
-        ],
-    ];
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
