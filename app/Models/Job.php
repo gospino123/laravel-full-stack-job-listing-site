b1fa00c51911184d68932bd7bf8eed0f08de44f8
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-08 12:41:47 -0400

    Prep(Job):Remove code to refactor

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,36 +1,10 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'title' => 'Director',
-                'salary' => '60,000',
-                'id' => 1,
-            ],
-            [
-                'title' => 'Programmer',
-                'salary' => '10,000',
-                'id' => 2,
-            ],
-            [
-                'title' => 'Teacher',
-                'salary' => '40,000',
-                'id' => 3,
-            ],
-        ];
-    }
-
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+class Job extends Model {
+    
 }
