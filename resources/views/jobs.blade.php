10ed7d35cc6a47dc59e0d13e83c493fe920e5b26
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-09 10:53:40 -0400

    Fix(jobs):Remove prepended $

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,7 +7,7 @@
         @foreach ($jobs as $job)
             <li>
                 <a class="text-lime-300 hover:underline" href="/jobs/{{ $job['id'] }}"
-                <strong>{{$job['title']}}</strong>: Pays ${{$job['salary']}} per year.
+                <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year.
             </li>
         @endforeach
     </ul>
