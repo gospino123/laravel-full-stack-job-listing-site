668b78a8f09e4a619061e9d0acd00edfb28db1c4
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-15 16:49:35 -0400

    Practice(setup):Save log for git checkout

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,3 +12,10 @@ class Job extends Model
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
 }
+
+/*
+c71e316 (HEAD -> main) Lesson12(pivot_table):Create,config db pivot
+d8142f0 Lesson12.1(pivot_tables):Prep laravel side
+4b98640 (practice-has-belongs-to-06152024) Lesson 11(model,db):Part2 - belongsTo,has relation
+94bc919 Lesson10(models,factories,db):Create part 1
+*/
