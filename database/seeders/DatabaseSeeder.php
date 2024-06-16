95d6afb4f78d709b0c679cabec54471200ac8e41
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-15 23:59:27 -0400

    Feat(seed):Config seed
     -php artisan db:seed
     -php artisan migrate:fresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
         ]);
+
+        Job::factory(100)->create();
+
+        // php artisan make:seeder
+            // Run different seeders in isolation
+            // Recreate world from seeder for test
+        // Make sure to clear before creating in order to avoid duplication preventing a seed
     }
 }
