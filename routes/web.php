e0c5be219b4e342a7d26e0306377928e4e556cbb
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-15 21:55:31 -0400

    Fix(lazy-loading):Revert back to eager loading

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/jobs', function () {
     // Use eager loading 'with'
     // get() will get ALL records, so you would want to limit this
     // Lazy loading has risk of n+1 and sometimes ppl disable at start of project
-    // $jobs = Job::with('employer')->get();
-    $jobs = Job::all();
+    $jobs = Job::with('employer')->get();
     
     return view('jobs', 
         [ 'jobs' => $jobs,
