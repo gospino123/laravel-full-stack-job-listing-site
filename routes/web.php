2fe7621b0785cf7e3ce8bfa1dcf59b50afedd8ee
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-22 16:43:15 -0400

    Refactor(routes,controller): Use convention, rm comments

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
@@ -12,104 +13,26 @@ Route::get('/', function () {
 });
 
 // Index
-Route::get('/jobs', function () {
-    // Use eager loading 'with'
-    
-    // Use cursorPaginate for best performance but bad URL, use case could be infinite scrolling
-    // $jobs = Job::with('employer')->cursorPaginate(3);
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    
-    return view('jobs.index', 
-        [ 'jobs' => $jobs,
-    ]);
-});
+Route::get('/jobs', [JobController::class, 'index']);
+// Route::method('/route', [SpecificController::class, 'methodOnClass']);
 
-// Be careful with hierarchy - wildcards (/jobs/{id}) should go towards the bottom
 // Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-/*
-Different ways to use Laravel's convention to get the right item
-1. Match wildcard {example} with argument ( $example) - and maybe usage below in the function block
-2. Use type, like the instance of a Job noted as (Job $job)
-3. Then, you don't have to use the findOrFail part
-Route::get('posts/{post:slug}');
-Route::get('posts/{post:id}');
-Route::get('posts/{post}');
-*/
+Route::get('/jobs/create', [JobController::class, 'create']);
 
 // Show
-Route::get('/jobs/{job}', function (Job $job) {
-    return view('jobs.show', ['job' => $job,]);
-});
+Route::get('/jobs/{job}', [JobController::class, 'show']);
 
 // Store
-Route::post('/jobs', function() {
-    // dd(request()->all());
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-        // laravel.com/docs/validation
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
+Route::post('/jobs', [JobController::class, 'store']);
 
 // Edit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    return view('jobs.edit', ['job' => $job,]);
-});
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
 
 // Update
-Route::patch('/jobs/{job}', function (Job $job) {
-    // Authorize (on hold...)
-    // Validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-    // Update job
-    $job->title = request('title');
-    $job->salary = request('salary');
-    /*
-    OR 
-    $job->update([
-        'title' => request('title');
-        'salary' => request('salary');
-    ])
-    */
-    // Persist
-    $job->save();
-    // Redirect
-    return redirect('/jobs/' . $job->id);
-
-});
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
 
 // Destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-    // authorize (on hold...)
-    // delete job
-    // redirect
-
-    /*
-    $job = Job::findOrFail($id);
-    $job->delete();
-    OR
-    */
-    $job->delete();
-
-    return redirect('/jobs');
-});
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/contact', function () {
     return view('contact');
