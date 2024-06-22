1b10fc2674490ef2ef931f58970c19d450d31947
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-22 16:45:33 -0400

    Refactor(routes):Rm redundant comments

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,13 @@ Route::get('/', function () {
     ]);
 });
 
-// Index
 Route::get('/jobs', [JobController::class, 'index']);
 // Route::method('/route', [SpecificController::class, 'methodOnClass']);
-
-// Create
 Route::get('/jobs/create', [JobController::class, 'create']);
-
-// Show
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-// Store
 Route::post('/jobs', [JobController::class, 'store']);
-
-// Edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-// Update
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-// Destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/contact', function () {
