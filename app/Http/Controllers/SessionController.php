3bfe0452d5624dff7cf19b563772639ccf58020b
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-23 14:55:40 -0400

    Fix(session):Handle invalid login auth

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -17,7 +18,11 @@ class SessionController extends Controller
             'password' => ['required']
         ]);
         // attempt to log in user
-        Auth::attempt($attributes);
+        if (! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.'
+            ]);
+        }
         // regenerate session token
         request()->session()->regenerate();
         // redirect to jobs
