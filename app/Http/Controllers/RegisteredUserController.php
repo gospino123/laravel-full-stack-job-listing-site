267fe4f38cc142a1cabb4ac11c63045fb5ff0c9f
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-23 14:13:38 -0400

    WIP(register):Set store - validation

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
     public function create() {
         return view('auth.register');
     }
+    // Validation - Laravel's Password
+    // can put this in service provider if used everywhere in app
+    // 'password' => ['required', Password::min(8)->letters()->numbers()->symbols()->mixedCase()->max(32)],
+    // 'confirmed' - follow conventions with '_confirmation'
     public function store() {
         // validate
+        request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email', 'max:254'],
+            'password' => ['required', Password::default(), 'confirmed'],
+        ]);
         // create user in database
         // log in user
         // redirect user to dashboard/homepage
