9dd4bcc8d6a5229a5b91f17ef773dbfb971b1d94
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-22 22:23:14 -0400

    Refactor(create):Use form components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -14,21 +14,17 @@
                     <x-form-field>
                         <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <x-form-input name="title" id="title" placeholder="Architect" required />
+                            <x-form-input name="title" id="title" placeholder="CEO" required />
                             <x-form-error name='title' />
                         </div>
                     </x-form-field>
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 per year" required>
-                            </div>
-                            @error('salary')
-                                <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input name="salary" id="salary" placeholder="$50,000" required />
+                            <x-form-error name='salary' />
                         </div>
-                    </div>
+                    </x-form-field>
                 </div>
 
                 {{-- <div class="mt-4">
