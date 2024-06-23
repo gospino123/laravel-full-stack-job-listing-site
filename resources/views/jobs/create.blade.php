49b59089436462a03d11a740421d3af7d0dd84be
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-22 22:21:03 -0400

    Feat(components):Refactor form w components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -11,17 +11,13 @@
                 <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
         
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Architect" required>
-                            </div>
-                            @error('title')
-                                <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input name="title" id="title" placeholder="Architect" required />
+                            <x-form-error name='title' />
                         </div>
-                    </div>
+                    </x-form-field>
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                         <div class="mt-2">
