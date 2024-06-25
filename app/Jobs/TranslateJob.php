3325c37d79552da6cb1860cbeb8e75ee4ba26d4a
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-24 22:51:21 -0400

    Feat(Job):Handle longer jobs in queue

---
+++ app/Jobs/TranslateJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Job;
+
+class TranslateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // php artisan make:job
+        // job logic is triggered here
+        // AI::translate($this->jobListing->description, 'spanish');
+        logger('Translating ' . $this->jobListing->title . ' to Spanish');
+        // Make sure to restart worker on production instance!!
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 
 // Create dummy route for testing
 Route::get('test', function() {
-  dispatch(function() {
-    logger('hello from the queue');
-  })->delay(5);
+  $job = Job::first();
+  TranslateJob::dispatch($job);
   return 'Done';
 });
 
