b7b37b71c830679db0886190dc70239e0896fffb
gizmo <72733125+gospino123@users.noreply.github.com>
2024-06-24 23:17:23 -0400

    Feat(SPA):Use best practice for SPA CSS

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        @vite(['resources/css/app.css'])
         @vite(['resources/js/app.js'])
         {{-- Now, we can use HMR for CSS and JS --}}
 
